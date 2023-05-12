8ce85343d76fec9c124e06bf93214182a9e56a47
EarlHikky <101412518+EarlHikky@users.noreply.github.com>
2023-05-12 15:11:36 +0300

    добавлены аргументы командной строки

--- tululu.py
+++ tululu.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import argparse
 
 from pathlib import Path
 from pathvalidate import sanitize_filename
@@ -20,7 +21,7 @@ def download_comments(comments, book_id, folder='comments/'):
             file.write(comment + '\n')
 
 
-def download_txt(url, filename, folder='books/'):
+def download_txt(url, filename, folder):
     response = requests.get(url)
     response.raise_for_status()
     Path(folder).mkdir(parents=True, exist_ok=True)
@@ -41,7 +42,12 @@ def download_image(url, folder):
         file.write(response.content)
 
 
-def check_img_exist(url, folder):
+def check_txt_exists(filename, folder):
+    file = os.path.join(folder, filename + '.txt')
+    return os.path.isfile(file)
+
+
+def check_img_exists(url, folder):
     unquoted_url = unquote(url)
     path = urlparse(unquoted_url).path
     filename = path.rstrip("/").split("/")[-1]
@@ -51,27 +57,34 @@ def check_img_exist(url, folder):
 
 def get_book(book_id):
     img_folder = 'images/'
-    url = f'https://tululu.org/txt.php?id={book_id}'
-    response = requests.get(url)
+    book_folder = 'books/'
+    download_url = f'https://tululu.org/txt.php?id={book_id}'
+    response = requests.get(download_url)
     response.raise_for_status()
-
     try:
         check_for_redirect(response)
-        book_title, img_url, comments, genres = get_book_info(book_id)
+
+        book = parse_book_page(book_id)
+        book_title = book['title']
+        img_url = book['img']
+        comments = book['comments']
+        genres = book['genres']
+        author = book['author']
         filename = f'{book_id}. {book_title}'
-        download_txt(url, filename)
-        if not check_img_exist(img_url, img_folder):
+        if not check_txt_exists(filename, book_folder):
+            download_txt(download_url, filename, book_folder)
+        if not check_img_exists(img_url, img_folder):
             download_image(img_url, img_folder)
         if comments:
             download_comments(comments, book_id)
 
-        return book_title, genres
+        return book_title, genres, author
 
     except HTTPError as e:
-        return str(e)
+        print(e)
 
 
-def get_book_info(book_id):
+def parse_book_page(book_id):
     url = f'https://tululu.org/b{book_id}'
     response = requests.get(url)
     response.raise_for_status()
@@ -80,23 +93,44 @@ def get_book_info(book_id):
     title_tag = soup.find(id='content').find('h1').text
     title = title_tag.split('::')[0].strip()
 
+    author = title_tag.split('::')[1].strip()
+
     img_url_tag = soup.find('div', class_='bookimage').find('img')['src']
     img_url = urljoin(url, img_url_tag)
 
     comments_tags = soup.find_all('div', class_='texts')
-    comments = []
-    for comment in comments_tags:
-        comments.append(comment.find('span').text)
+    comments = [comment.find('span').text for comment in comments_tags]
 
     genre_tags = soup.find(id='content').find('span', class_='d_book').find_all('a')
     genres = list(genre.text for genre in genre_tags)
 
-    return title, img_url, comments, genres
+    book_info = {'title': title, 'comments': comments,
+                 'genres': genres, 'img': img_url, 'author': author}
+
+    return book_info
+
+
+def print_book_info(book_id):
+    book_title, genres, author = get_book(book_id)
+    print(f'Название: {book_title}',
+          f'Автор: {author}',
+          f'Жанр: {genres}',
+          sep='\n', end='\n\n')
 
 
 def main():
-    book_title, genre = get_book(1)
-    print(f'Заголовок: {book_title}', genre, sep='\n')
+    parser = argparse.ArgumentParser(description='Загружает книги с сайта https://tululu.org')
+    parser.add_argument('start_id', type=int,
+                        help='Стартовый id книги')
+    parser.add_argument('end_id', type=int,
+                        help='Конечный id книги')
+    args = parser.parse_args()
+
+    for book_id in range(args.start_id, args.end_id + 1):
+        try:
+            print_book_info(book_id)
+        except ValueError:
+            print(f'Книга c id {book_id} не найдена', end='\n\n')
 
 
 if __name__ == '__main__':
